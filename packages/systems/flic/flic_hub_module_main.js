2c92d7c5ebead1995bb0525dc648d850476736e4
B-Hartley <Bruce.hartley@clickspares.co.uk>
2021-07-16 09:14:30 +0100

    fix button names if non friendly

--- packages/systems/flic/flic_hub_module_main.js
+++ packages/systems/flic/flic_hub_module_main.js
@@ -6,12 +6,13 @@ var url = "http://192.168.11.124:8123/api/webhook/flic-abcd1234";
 buttonManager.on("buttonSingleOrDoubleClickOrHold", function(obj) {
 	var button = buttonManager.getButton(obj.bdaddr);
 	var clickType = obj.isSingleClick ? "click" : obj.isDoubleClick ? "double_click" : "hold";
+    var buttonId = button.name.replace(" ", "_").toLowerCase();	
 	
 	http.makeRequest({
 		url: url,
 		method: "POST",
 		headers: {"Content-Type": "application/json"},		
-		content: JSON.stringify({"button_name": button.name, "click_type": clickType, "battery_status": button.batteryStatus }),				
+		content: JSON.stringify({"button_name": button.name, "button_id": buttonId, "click_type": clickType, "battery_status": button.batteryStatus }),				
 	}, function(err, res) {
 //		console.log("request status: " + res.statusCode);
 //		console.log("button_name: " + button.name)
